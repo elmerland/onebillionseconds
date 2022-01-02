65a941cbecd2ed3136d764c1b0dbfbfe4430c4f8
Elmer Landaverde <elmerlandaverde@gmail.com>
2022-01-02 00:48:11 -0600

    Add css styling

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,7 @@ console.log("hello again");
 
 const dt = DateTime.now()
 const oneBillionSecondsAgo = dt.minus({ seconds: 1000000000})
-console.log(oneBillionSecondsAgo.toString());
+const oneBillionSecondsFromNow = dt.plus({ seconds: 1000000000 })
 
 document.getElementById("onebillionsecondsago").innerHTML = oneBillionSecondsAgo.toLocaleString(DateTime.DATETIME_HUGE);
+document.getElementById("onebillionsecondsfromnow").innerHTML = oneBillionSecondsFromNow.toLocaleString(DateTime.DATETIME_HUGE);
