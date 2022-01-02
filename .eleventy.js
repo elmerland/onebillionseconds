2ed867a64b2b270bbc2cc1d81ddb4fb024a59dba
Elmer <5368806+elmerland@users.noreply.github.com>
2022-01-02 00:52:37 -0600

    Add css styling (#4)
    * Add css styling
    
    * Tweak to title
    
    Co-authored-by: Elmer Landaverde <elmerlandaverde@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets/css");
+    eleventyConfig.addWatchTarget("src/assets/css")
     return {
         dir: {
             input: "src",
+            includes: "_includes",
+            data: "_data",
             output: "_site"
         },
-        htmlTemplateEngine: "liquid",
     }
 }
