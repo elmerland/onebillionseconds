b76fdd549c865d7014b349db2065fc56b22ecfba
Elmer Landaverde <elmerlandaverde@gmail.com>
2022-01-06 19:59:46 -0600

    Add more widgets

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import {App, ProgressBar, SecondsCalculator} from "./App";
+import React from "react";
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+ReactDOM.render(<ProgressBar />, document.getElementById("oneSecondAnimation"));
+
+ReactDOM.render(<SecondsCalculator />, document.getElementById("secondsCalculator"));
