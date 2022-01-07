ae6edabba1469b8593a468f01adcbd07e6d86cbc
Elmer <5368806+elmerland@users.noreply.github.com>
2022-01-06 20:10:30 -0600

    Add more widgets (#6)
    Co-authored-by: Elmer Landaverde <elmerlandaverde@gmail.com>

--- src/assets/js/App.js
+++ src/assets/js/App.js
@@ -1,10 +1,110 @@
 import React, { useState, useEffect } from "react";
 import { DateTime } from "luxon";
 
-const dt = DateTime.now()
-const formatString = "yyyy LLL dd HH:mm:ss '('cccc')'"
-const oneHundredYearsIsh = 3155760000
-const oneBillion = 1000000000
+const formatString = "yyyy LLL dd HH:mm:ss '('cccc')'";
+const oneBillion = 1000000000;
+
+const oneYearInSeconds = 31556952;
+const oneCenturyInSeconds = oneYearInSeconds * 100;
+const oneDayInSeconds = 86400
+const oneHourInSeconds = 3600;
+const oneMinuteInSeconds = 60;
+
+function secondsToDuration(seconds) {
+    let availSeconds = seconds
+
+    const numCenturies = Math.floor(availSeconds / oneCenturyInSeconds);
+    availSeconds = availSeconds % oneCenturyInSeconds;
+    const numYears = Math.floor(availSeconds / oneYearInSeconds);
+    availSeconds = availSeconds % oneYearInSeconds;
+    const numDays = Math.floor(availSeconds / oneDayInSeconds);
+    availSeconds = availSeconds % oneDayInSeconds;
+    const numHours = Math.floor(availSeconds / oneHourInSeconds);
+    availSeconds = availSeconds % oneHourInSeconds;
+    const numMinutes = Math.floor(availSeconds / oneMinuteInSeconds);
+    availSeconds = availSeconds % oneMinuteInSeconds;
+
+    const centuryLabel = numCenturies > 1 ? 'centuries' : 'century';
+    const yearsLabel = numYears > 1 ? 'years' : 'year';
+    const daysLabel = numDays > 1 ? 'days' : 'day';
+    const hoursLabel = numHours > 1 ? 'hours' : 'hour';
+    const minutesLabel = numMinutes > 1 ? 'minutes' : 'minute';
+    const secondsLabel = availSeconds > 1 ? 'seconds' : 'second';
+
+    let output = [];
+    if (numCenturies > 0) { output.push(`${numCenturies} ${centuryLabel}`)}
+    if (numYears > 0) { output.push(`${numYears} ${yearsLabel}`)}
+    if (numDays > 0) { output.push(`${numDays} ${daysLabel}`)}
+    if (numHours > 0) { output.push(`${numHours} ${hoursLabel}`)}
+    if (numMinutes > 0) { output.push(`${numMinutes} ${minutesLabel}`)}
+    if (availSeconds > 0) { output.push(`${availSeconds} ${secondsLabel}`)}
+
+    return output.join(' ')
+}
+
+export function SecondsCalculator() {
+    const [inputState, setInputState] = useState(1000000000)
+
+    const updateInput = (event) => {
+        let intValue = parseInt(event.target.value)
+        if (isNaN(intValue)) {
+            intValue = 0;
+        }
+        setInputState(intValue);
+    }
+    return (<div className="field-body">
+        <div className="field is-narrow">
+            <label className="label">Seconds</label>
+            <div className="control">
+                <input className="input" type="text" value={inputState} onChange={updateInput} />
+            </div>
+            <p className="help">{inputState.toLocaleString()}</p>
+        </div>
+        <div className="field is-expanded">
+            <label className="label">Equivalent</label>
+            <div className="control">
+                <input className="input is-static has-text-weight-semibold is-family-monospace" type="text" value={secondsToDuration(inputState)} readOnly/>
+            </div>
+        </div>
+    </div>);
+}
+
+export function ProgressBar() {
+    const [progressVal, setProgressVal] = useState(0);
+    const [playState, setPlayState] = useState(false)
+    useEffect(() => {
+        if (!playState ) {
+            // Only start timer if play status is true
+            return;
+        }
+        const interval = setInterval(() => {
+            setProgressVal(progressVal => {
+                if (progressVal >= 100) {
+                    setPlayState(false);
+                }
+                return (progressVal + 1)
+            });
+        }, 10);
+        return () => clearInterval(interval)
+    }, [playState])
+
+    const togglePlay = () => {
+        // Reset progress bar when play status will be set to true
+        if (!playState) {
+            setProgressVal(0);
+        }
+        setPlayState(!playState)
+    }
+
+    return (<div className="is-flex is-justify-content-left is-align-items-center" style={{ height: '3em' }}>
+            {!playState &&
+                <button className="button mr-3" onClick={togglePlay}>
+                    {"Show me one second!"}
+                </button>
+            }
+            {playState && <progress className="progress is-large is-flex-grow-1" value={progressVal.toString()} max="100">{progressVal}</progress>}
+    </div>);
+}
 
 function LiveCounter() {
     const [dateState, setDateState] = useState(DateTime.now())
@@ -16,7 +116,7 @@ function LiveCounter() {
         setInterval(() => setDateState(DateTime.now()), 500);
     }, [])
 
-    return <div className="section px-0 pt-0">
+    return <div>
         <p>One billion seconds ago:</p>
         <p className="is-size-5 has-text-weight-semibold is-family-monospace">{oneBillionSecondsAgo}</p>
         <br></br>
@@ -27,7 +127,7 @@ function LiveCounter() {
 
 function AgeCalculator() {
     const [stableDateState] = useState(DateTime.now())
-    const randomBirthday = DateTime.now().minus({ seconds: getRandomInt(0, oneHundredYearsIsh)});
+    const randomBirthday = DateTime.now().minus({ seconds: getRandomInt(0, oneCenturyInSeconds)});
     const [birthdayState, setBirthdayState] = useState({year: randomBirthday.year, month: randomBirthday.month, day: randomBirthday.day})
 
     const updateYear = (event) => setBirthdayState({...birthdayState, year: event.target.value})
@@ -50,7 +150,7 @@ function AgeCalculator() {
     }
 
 
-    return <div className="section px-0 pt-0">
+    return <div>
         <p>If you were born on <span className="is-size-5 has-text-weight-semibold is-family-monospace">{birthdayDateFormat}</span> you are <span className="is-size-5 has-text-weight-semibold is-family-monospace">{ageInBillionSeconds} billion seconds old</span></p>
         <br></br>
         <div className="container is-max-desktop mx-0">
@@ -78,9 +178,10 @@ function AgeCalculator() {
     </div>
 }
 
-export default function App() {
+export function App() {
     return <div>
         <LiveCounter/>
+        <br/>
         <AgeCalculator/>
     </div>
 
